d997ef3c5cfcab5fe1d5644d0a57eeb01d7c0370
Vedovelli <vedovelli@gmail.com>
2015-01-04 18:08:27 -0200

    Implementacao inicial da interface de usuários está completa. Protótipo

--- app/views/angular/strictly_angular.php
+++ app/views/angular/strictly_angular.php
@@ -1,3 +1,210 @@
 <div ng-app>
+<!--Filters-->
+	<h3>Filtros</h3>
+	<div class="row">
+		<div class="col-md-6">
+			<form action="#">
+				<div class="form-group">
+					<label for="selectCity">filtre por Cidade</label>
+					<select name="city_filter" id="selectCity" multiple>
+						<option value="Santo André">Santo André</option>
+						<option value="São Paulo">São Paulo</option>
+						<option value="São Caetano do Sul">São Caetano do Sul</option>
+					</select>
+				</div>
+			</form>
+		</div>
+		<div class="col-md-4">
+			<form action="#">
+				<div class="form-group">
+					<label for="selectOrdenacao">ordenar por</label>
+					<select name="city_filter" id="selectOrdenacao">
+						<option selected></option>
+						<option value="1">por Nome descendente</option>
+						<option value="2">por Nome ascendente</option>
+						<option value="3">por Atualização descendente</option>
+						<option value="4">por Atualização ascendente</option>
+					</select>
+				</div>
+			</form>
+		</div>
+		<div class="col-md-2">
+			<div class="form-group spa-container-btn-novo">
+				<button class="btn btn-primary">
+					<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+					Criar novo usuário</button>
+			</div>
+		</div>
+	</div>
+<!--/Filters-->
 
+
+<!--User List-->
+	<div class="spa-user-list">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="panel panel-info spa-user-item">
+					<div class="panel-heading">
+						<h3 class="panel-title">Fábio Vedovelli
+							<small class="pull-right">vedovelli@gmail.com</small></h3>
+					</div>
+					<div class="panel-body spa-user-card-body">
+						<img src="/img/ved_oficial_2015_headshot.jpg" alt="Fábio Vedovelli" class="pull-right" width="90">
+						<strong>Cidade:</strong> Santo André<br>
+						<strong>Estado:	</strong> SP<br><br>
+						<p class="text-center"><small><em>Atualizado em 29/12/2014 as 22:45</em></small></p>
+						<div class="spa-user-actions">
+							<button type="button" data-fullname="Joaquim" class="btn btn-success btn-xs spa-edit-user">
+								<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Editar
+							</button>
+							<button type="button" class="btn btn-danger btn-xs spa-remove-user">
+								<span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Excluir
+							</button>
+						</div>
+					</div> <!--.panel-body-->
+				</div> <!--.panel panel-info spa-user-item-->
+			</div> <!--.col-md-4-->
+			<div class="col-md-4">
+				<div class="panel panel-info spa-user-item">
+					<div class="panel-heading">
+						<h3 class="panel-title">Fábio Vedovelli
+							<small class="pull-right">vedovelli@gmail.com</small></h3>
+					</div>
+					<div class="panel-body spa-user-card-body">
+						<img src="/img/ved_oficial_2015_headshot.jpg" alt="Fábio Vedovelli" class="pull-right" width="90">
+						<strong>Cidade:</strong> Santo André<br>
+						<strong>Estado:	</strong> SP<br><br>
+						<p class="text-center"><small><em>Atualizado em 29/12/2014 as 22:45</em></small></p>
+						<div class="spa-user-actions">
+							<button type="button" data-fullname="Joaquim" class="btn btn-success btn-xs spa-edit-user">
+								<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Editar
+							</button>
+							<button type="button" class="btn btn-danger btn-xs spa-remove-user">
+								<span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Excluir
+							</button>
+						</div>
+					</div> <!--.panel-body-->
+				</div> <!--.panel panel-info spa-user-item-->
+			</div> <!--.col-md-4-->
+			<div class="col-md-4">
+				<div class="panel panel-info spa-user-item">
+					<div class="panel-heading">
+						<h3 class="panel-title">Fábio Vedovelli
+							<small class="pull-right">vedovelli@gmail.com</small></h3>
+					</div>
+					<div class="panel-body spa-user-card-body">
+						<img src="/img/ved_oficial_2015_headshot.jpg" alt="Fábio Vedovelli" class="pull-right" width="90">
+						<strong>Cidade:</strong> Santo André<br>
+						<strong>Estado:	</strong> SP<br><br>
+						<p class="text-center"><small><em>Atualizado em 29/12/2014 as 22:45</em></small></p>
+						<div class="spa-user-actions">
+							<button type="button" data-fullname="Joaquim" class="btn btn-success btn-xs spa-edit-user">
+								<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Editar
+							</button>
+							<button type="button" class="btn btn-danger btn-xs spa-remove-user">
+								<span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Excluir
+							</button>
+						</div>
+					</div> <!--.panel-body-->
+				</div> <!--.panel panel-info spa-user-item-->
+			</div> <!--.col-md-4-->
+		</div> <!--.row-->
+		<div class="row">
+			<div class="col-md-4">
+				<div class="panel panel-info spa-user-item">
+					<div class="panel-heading">
+						<h3 class="panel-title">Fábio Vedovelli
+							<small class="pull-right">vedovelli@gmail.com</small></h3>
+					</div>
+					<div class="panel-body spa-user-card-body">
+						<img src="/img/ved_oficial_2015_headshot.jpg" alt="Fábio Vedovelli" class="pull-right" width="90">
+						<strong>Cidade:</strong> Santo André<br>
+						<strong>Estado:	</strong> SP<br><br>
+						<p class="text-center"><small><em>Atualizado em 29/12/2014 as 22:45</em></small></p>
+						<div class="spa-user-actions">
+							<button type="button" data-fullname="Joaquim" class="btn btn-success btn-xs spa-edit-user">
+								<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Editar
+							</button>
+							<button type="button" class="btn btn-danger btn-xs spa-remove-user">
+								<span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Excluir
+							</button>
+						</div>
+					</div> <!--.panel-body-->
+				</div> <!--.panel panel-info spa-user-item-->
+			</div> <!--.col-md-4-->
+			<div class="col-md-4">
+				<div class="panel panel-info spa-user-item">
+					<div class="panel-heading">
+						<h3 class="panel-title">Fábio Vedovelli
+							<small class="pull-right">vedovelli@gmail.com</small></h3>
+					</div>
+					<div class="panel-body spa-user-card-body">
+						<img src="/img/ved_oficial_2015_headshot.jpg" alt="Fábio Vedovelli" class="pull-right" width="90">
+						<strong>Cidade:</strong> Santo André<br>
+						<strong>Estado:	</strong> SP<br><br>
+						<p class="text-center"><small><em>Atualizado em 29/12/2014 as 22:45</em></small></p>
+						<div class="spa-user-actions">
+							<button type="button" data-fullname="Joaquim" class="btn btn-success btn-xs spa-edit-user">
+								<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Editar
+							</button>
+							<button type="button" class="btn btn-danger btn-xs spa-remove-user">
+								<span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Excluir
+							</button>
+						</div>
+					</div> <!--.panel-body-->
+				</div> <!--.panel panel-info spa-user-item-->
+			</div> <!--.col-md-4-->
+			<div class="col-md-4">
+				<div class="panel panel-info spa-user-item">
+					<div class="panel-heading">
+						<h3 class="panel-title">Fábio Vedovelli
+							<small class="pull-right">vedovelli@gmail.com</small></h3>
+					</div>
+					<div class="panel-body spa-user-card-body">
+						<img src="/img/ved_oficial_2015_headshot.jpg" alt="Fábio Vedovelli" class="pull-right" width="90">
+						<strong>Cidade:</strong> Santo André<br>
+						<strong>Estado:	</strong> SP<br><br>
+						<p class="text-center"><small><em>Atualizado em 29/12/2014 as 22:45</em></small></p>
+						<div class="spa-user-actions">
+							<button type="button" data-fullname="Joaquim" class="btn btn-success btn-xs spa-edit-user">
+								<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Editar
+							</button>
+							<button type="button" class="btn btn-danger btn-xs spa-remove-user">
+								<span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Excluir
+							</button>
+						</div>
+					</div> <!--.panel-body-->
+				</div> <!--.panel panel-info spa-user-item-->
+			</div> <!--.col-md-4-->
+		</div> <!--.row-->
+	</div> <!--.spa-user-list-->
+<!--/User List-->
+
+<!--Pagination-->
+	<nav class="text-center">
+		<ul class="pagination">
+			<li>
+				<a href="#" aria-label="Previous">
+				<span aria-hidden="true">&laquo;</span>
+				</a>
+			</li>
+			<li><a href="#">1</a></li>
+			<li class="active"><a href="#">2</a></li>
+			<li><a href="#">3</a></li>
+			<li><a href="#">4</a></li>
+			<li><a href="#">5</a></li>
+			<li><a href="#">6</a></li>
+			<li><a href="#">7</a></li>
+			<li><a href="#">8</a></li>
+			<li><a href="#">9</a></li>
+			<li><a href="#">10</a></li>
+			<li><a href="#">11</a></li>
+			<li>
+				<a href="#" aria-label="Next">
+				<span aria-hidden="true">&raquo;</span>
+			</a>
+			</li>
+		</ul>
+	</nav>
+<!--/Pagination-->
 </div>
--- public/js/spa.js
+++ public/js/spa.js
@@ -35,7 +35,7 @@
 		
 			bootbox.dialog({
 				title: 'Editar Usuário ' + fullname,
-				message: 'Adicionar aqui o form'
+				message: userForm
 			});
 		});
 
@@ -43,7 +43,7 @@
 		
 			bootbox.dialog({
 				title: 'Criar novo usuário',
-				message: 'Adicionar aqui o form'
+				message: userForm
 			});
 		});
 
@@ -61,6 +61,78 @@
 		{
 			$txtEmail.focus();
 		}
+
+
+
+
+		var userForm = '
+	<form action="#" class="spa-user-form">
+		
+		<img src="/img/ved_oficial_2015_headshot.jpg" alt="Fabio Vedovelli" width="200" class="spa-user-form-image">
+		
+		<h4>Todos os campos são de preenchimento obrigatório</h4>
+		
+		<div class="row">
+			<div class="col-md-5">
+				<div class="form-group">
+					<label for="txtFullname">Nome Completo</label>
+					<input type="text" class="form-control" name="fullname" id="txtFullname" required>
+				</div>		
+			</div>
+			<div class="col-md-4">
+				<div class="form-group">
+					<label for="txtEmail">E-mail</label>
+					<input type="email" class="form-control" name="email" id="txtEmail" required>
+				</div>
+			</div>
+			<div class="col-md-offset-3"></div>
+		</div>
+		<div class="row">
+			<div class="col-md-3">
+				<div class="form-group">
+					<label for="txtUsername">Usuário</label>
+					<input type="text" class="form-control" name="username" id="txtUsername" required>
+				</div>		
+			</div>
+			<div class="col-md-3">
+				<div class="form-group">
+					<label for="txtPassword">Senha</label>
+					<input type="password" class="form-control" name="password" id="txtPassword" required>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="form-group">
+					<label for="txtPasswordConfirmation">Confirmação de Senha</label>
+					<input type="password" class="form-control" name="password_confirmation" id="txtPasswordConfirmation" required>
+				</div>
+			</div>
+			<div class="col-md-offset-3"></div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-2">
+				<div class="form-group">
+					<label for="txtCep">CEP</label>
+					<input type="text" class="form-control" name="cep" id="txtCep" required>
+				</div>		
+			</div>
+			<div class="col-md-5">
+				<div class="form-group">
+					<label for="txtCidade">Cidade</label>
+					<input type="text" class="form-control" name="cidade" id="txtCidade" required>
+				</div>
+			</div>
+			<div class="col-md-2">
+				<div class="form-group">
+					<label for="txtEstado">UF</label>
+					<input type="text" class="form-control" name="estado" id="txtEstado" required>
+				</div>
+			</div>
+			<div class="col-md-offset-3"></div>
+		</div>
+		
+	</form>
+				';
 		
 	});
 	
