427a116accf1b78d91dbcb0e4c2443f55fa0db60
Vedovelli <vedovelli@gmail.com>
2015-01-04 15:18:22 -0200

    implementação inicial do modal

--- app/views/angular/angular.blade.php
+++ app/views/angular/angular.blade.php
@@ -4,6 +4,27 @@
 	<!-- @include('angular.strictly_angular') -->
 	
 
+<!--Modal-->
+	<div class="modal fade" id="userModal">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title">Modal title</h4>
+				</div>
+				<div class="modal-body">
+					<p>One fine body&hellip;</p>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary">Save changes</button>
+				</div>
+			</div><!-- /.modal-content -->
+		</div><!-- /.modal-dialog -->
+	</div><!-- /.modal -->
+<!--/Modal-->
+
+
 <!--Filters-->
 	<h3>Filtros</h3>
 	<div class="row">
@@ -35,7 +56,7 @@
 		</div>
 		<div class="col-md-2">
 			<div class="form-group spa-container-btn-novo">
-				<button class="btn btn-primary">Criar novo usuário</button>
+				<button class="btn btn-primary" data-toggle="modal" data-target="#userModal">Criar novo usuário</button>
 			</div>
 		</div>
 	</div>
@@ -196,7 +217,31 @@
 <!--/User List-->
 
 <!--Pagination-->
-
+	<nav class="text-center">
+		<ul class="pagination">
+			<li>
+				<a href="#" aria-label="Previous">
+				<span aria-hidden="true">&laquo;</span>
+				</a>
+			</li>
+			<li><a href="#">1</a></li>
+			<li class="active"><a href="#">2</a></li>
+			<li><a href="#">3</a></li>
+			<li><a href="#">4</a></li>
+			<li><a href="#">5</a></li>
+			<li><a href="#">6</a></li>
+			<li><a href="#">7</a></li>
+			<li><a href="#">8</a></li>
+			<li><a href="#">9</a></li>
+			<li><a href="#">10</a></li>
+			<li><a href="#">11</a></li>
+			<li>
+				<a href="#" aria-label="Next">
+				<span aria-hidden="true">&raquo;</span>
+			</a>
+			</li>
+		</ul>
+	</nav>
 <!--/Pagination-->
 
 
--- public/js/spa.js
+++ public/js/spa.js
@@ -1,5 +1,7 @@
 (function($) { // Se isola a variável $ dentro de um escopo fechado
 	
+	'use strict';
+
 	$(document).ready(function() { // Se inicia o uso da jQuery apenas após o DOM ter sido totalmente carregado
 	
 		var 	$txtEmail = $('#txtEmail'),
